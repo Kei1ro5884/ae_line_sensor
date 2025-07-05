04bd3a4bd8e9895164b968b1ee3d446f4fd2b97e
Keiichiro Kobayashi <s23c1050eb@s.chibakoudai.jp>
2025-07-05 23:13:25 +0900

    commit

--- launch/line_sensor.launch.py
+++ launch/line_sensor.launch.py
@@ -8,46 +8,57 @@ import os
 
 
 def generate_launch_description():
-    pkg = get_package_share_directory('ae_line_sensor')
-    xacro_file = os.path.join(pkg, 'urdf', 'ae_njl5901ar_8ch.xacro')
+    share = get_package_share_directory('ae_line_sensor')
+    xacro_file = os.path.join(share, 'urdf', 'ae_njl5901ar_8ch.xacro')
 
-    # Gazebo プラグイン検索パス
-    env = SetEnvironmentVariable(
+    # Gazebo プラグイン検索パスを追加
+    plugin_env = SetEnvironmentVariable(
         'GAZEBO_PLUGIN_PATH',
         '/opt/ros/humble/lib:' + os.environ.get('GAZEBO_PLUGIN_PATH', '')
     )
 
     # gzserver + factory + init
-    server = ExecuteProcess(
+    gzserver = ExecuteProcess(
         cmd=['gzserver', '--verbose',
              '-s', 'libgazebo_ros_init.so',
              '-s', 'libgazebo_ros_factory.so'],
         output='screen')
 
-    client = ExecuteProcess(cmd=['gzclient'], output='screen')
+    # GUI
+    gzclient = ExecuteProcess(cmd=['gzclient'], output='screen')
 
     # robot_description を publish
     rsp = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[{
-            'robot_description': Command([f'bash -c "xacro {xacro_file}"'])
+            'robot_description': Command(
+                [f'bash -c "xacro {xacro_file}"'])
         }],
         output='screen')
 
-    # ← spawn_entity.py は **オプション無し** が一番安全
-    spawn = ExecuteProcess(
-        cmd=[
-            'ros2', 'run', 'gazebo_ros', 'spawn_entity.py',
+    # モデルをスポーン（リマップ無し = デフォルト /spawn_entity）
+    spawn = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
             '-topic', 'robot_description',
             '-entity', 'line_sensor'
         ],
         output='screen')
 
+    # センサノード
     sensor = Node(
         package='ae_line_sensor',
         executable='line_sensor_node',
         output='screen')
 
-    return LaunchDescription([env, server, client, rsp, spawn, sensor])
+    return LaunchDescription([
+        plugin_env,
+        gzserver,
+        gzclient,
+        rsp,
+        spawn,
+        sensor
+    ])
 
